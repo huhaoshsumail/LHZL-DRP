2bfb9bf356853036049bc28e04a56cc0320958e8
YangZhouChaoFan <chenhao1000@live.cn>
2016-03-21 11:54:24 +0800

    创建项目

---
+++ LHZL-DRP-BE/src/main/java/com/lhzl/drp/interceptor/CorsFilter.java
@@ -0,0 +1,61 @@
+package com.lhzl.drp.interceptor;
+
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class CorsFilter implements Filter {
+
+    private String allowOrigin;
+    private String allowMethods;
+    private String allowCredentials;
+    private String allowHeaders;
+    private String exposeHeaders;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        allowOrigin = filterConfig.getInitParameter("allowOrigin");
+        allowMethods = filterConfig.getInitParameter("allowMethods");
+        allowCredentials = filterConfig.getInitParameter("allowCredentials");
+        allowHeaders = filterConfig.getInitParameter("allowHeaders");
+        exposeHeaders = filterConfig.getInitParameter("exposeHeaders");
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException, IOException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+        if (!StringUtils.isEmpty(allowOrigin)) {
+            List<String> allowOriginList = Arrays.asList(allowOrigin.split(","));
+            if (!CollectionUtils.isEmpty(allowOriginList)) {
+                String currentOrigin = request.getHeader("Origin");
+                if (allowOriginList.contains(currentOrigin)) {
+                    response.setHeader("Access-Control-Allow-Origin", currentOrigin);
+                }
+            }
+        }
+        if (StringUtils.isEmpty(allowMethods)) {
+            response.setHeader("Access-Control-Allow-Methods", allowMethods);
+        }
+        if (!StringUtils.isEmpty(allowCredentials)) {
+            response.setHeader("Access-Control-Allow-Credentials", allowCredentials);
+        }
+        if (!StringUtils.isEmpty(allowHeaders)) {
+            response.setHeader("Access-Control-Allow-Headers", allowHeaders);
+        }
+        if (!StringUtils.isEmpty(exposeHeaders)) {
+            response.setHeader("Access-Control-Expose-Headers", exposeHeaders);
+        }
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}  
---
+++ LHZL-DRP-BE/src/main/java/com/lhzl/drp/util/ExceptionAdvice.java
@@ -0,0 +1,59 @@
+package com.lhzl.drp.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+@ResponseBody
+public class ExceptionAdvice {
+
+    static private Logger logger = LoggerFactory.getLogger(ExceptionAdvice.class);
+
+    /**
+     * 400 - Bad Request
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public Response handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        logger.error("参数解析失败", e);
+        return new Response().failure("could_not_read_json");
+    }
+
+    /**
+     * 405 - Method Not Allowed
+     */
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public Response handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        logger.error("不支持当前请求方法", e);
+        return new Response().failure("request_method_not_supported");
+    }
+
+    /**
+     * 415 - Unsupported Media Type
+     */
+    @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public Response handleHttpMediaTypeNotSupportedException(Exception e) {
+        logger.error("不支持当前媒体类型", e);
+        return new Response().failure("content_type_not_supported");
+    }
+
+    /**
+     * 500 - Internal Server Error
+     */
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public Response handleException(Exception e) {
+        logger.error("服务运行异常", e);
+        return new Response().failure(e.getMessage());
+    }
+} 
