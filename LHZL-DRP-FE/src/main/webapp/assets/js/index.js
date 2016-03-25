5302bf9f0e6f3b950e28b4d0b8a134f9577c2d6d
YangZhouChaoFan <chenhao1000@live.cn>
2016-03-25 14:58:27 +0800

    创建项目

--- LHZL-DRP-FE/src/main/webapp/assets/js/index.js
+++ LHZL-DRP-FE/src/main/webapp/assets/js/index.js
@@ -352,7 +352,6 @@
     });
     $(document).on('pjax:complete', function () {
         //执行pjax结束，在这里添加要重载的代码，可自行添加loading动画结束或隐藏代码。例如NProgress的结束代码 NProgress.done();
-        debugger;
     });
 
 })(jQuery, $.AdminLTE);
