e58b785a7e0dc9583c19b6b259d8234070572924
YangZhouChaoFan <chenhao1000@live.cn>
2016-03-31 16:39:02 +0800

    创建项目

--- LHZL-DRP-FE/src/main/webapp/admin/pages/data/book/index.js
+++ LHZL-DRP-FE/src/main/webapp/admin/pages/data/book/index.js
@@ -4,6 +4,7 @@
 var initDataBook = function () {
     $("#codeTable").grid({
         columns: [
+            {name: "userid", display: "id", hidden: true},
             {name: "username", "display": "会员姓名"},
             {name: "mobile", "display": "会员电话", template: "<span style='color: red'>${parseInt(value)+1}</span>"},
         ],
