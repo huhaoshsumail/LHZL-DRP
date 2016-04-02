c3a24ca05ff71742b0122186c95e771de3759152
YangZhouChaoFan <chenhao1000@live.cn>
2016-04-02 21:10:43 +0800

    添加后台字典代码管理

---
+++ LHZL-DRP-BE/src/main/java/com/lhzl/drp/service/BookCodeServiceImpl.java
@@ -0,0 +1,43 @@
+package com.lhzl.drp.service;
+
+import com.lhzl.drp.dao.BookCodeMapper;
+import com.lhzl.drp.model.BookCode;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by chenhao on 2016/4/2.
+ */
+public class BookCodeServiceImpl implements BookCodeService {
+
+    @Autowired
+    public BookCodeMapper bookCodeMapper;
+
+    @Override
+    public BookCode queryBookCodeById(long codeid) {
+        return bookCodeMapper.selectByPrimaryKey(codeid);
+    }
+
+    @Override
+    public List<BookCode> queryBookCode(Map<String, Object> map) {
+        return bookCodeMapper.queryBookCode(map);
+    }
+
+    @Override
+    public void insertBookCode(BookCode bookCode) {
+        bookCodeMapper.insert(bookCode);
+    }
+
+    @Override
+    public void updateBookCode(BookCode bookCode) {
+        bookCodeMapper.updateByPrimaryKey(bookCode);
+    }
+
+    @Override
+    public void deleteBookCode(long codeid) {
+        bookCodeMapper.deleteByPrimaryKey(codeid);
+    }
+
+}
